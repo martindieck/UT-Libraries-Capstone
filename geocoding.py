f9c174708ca1fe2dfee908647d592e17e64d5ee1
martindieck <krutterlax@gmail.com>
2024-02-13 13:07:31 -0600

    Added Automatic Maps Link to verify

--- geocoding.py
+++ geocoding.py
@@ -10,13 +10,13 @@ def geocode(address):
     api_key = config.get('geocoding', 'API_KEY')
     encoded_address = quote(address)
     url = f"https://api.geoapify.com/v1/geocode/search?text={encoded_address}&apiKey={api_key}"
-    print(url)
     response = requests.get(url)
 
     if response.status_code == 200:
-        print("GET request successful")
-        print("Response body:")
-        print(response.text)
+        data = response.json()
+        long = data["features"][0]["geometry"]["coordinates"][0]
+        lat = data["features"][0]["geometry"]["coordinates"][1]
+        print(f"https://maps.google.com/?q={lat},{long}")
     else:
         print(f"GET request failed with status code {response.status_code}")
 
