5abe4d6e1409e48dc7aad977bfc8e398368166c4
martindieck <krutterlax@gmail.com>
2024-04-02 23:24:39 -0500

    Added Flask App for Coordinate Verification

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import csv
+import random
+import configparser
+
+app = Flask(__name__)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config.get('geocoding', 'GOOGLE_API_KEY')
+
+def read_csv(filename):
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    return data
+
+def write_csv(filename, data):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+
+@app.route('/')
+def index():
+    data = read_csv('data.csv')
+
+    filtered_data = [row for row in data if not row.get('Validation')]
+
+    if filtered_data:
+        random_index = random.randint(0, len(filtered_data) - 1)
+        random_row = filtered_data[random_index]
+        row_index = data.index(random_row)
+        message = str(len(filtered_data)) + " entries remaining"
+    else:
+        random_row = None
+        row_index = None
+        message = "All entries done! Congratulations!"
+
+    return render_template('index.html', row=random_row, row_index=row_index, message=message, api_key=api_key)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+
+    response = request.form['response']
+    row_id = int(request.form['row_id'])
+
+    data = read_csv('data.csv')
+
+    data[row_id]['Validation'] = response
+
+    write_csv('data.csv', data)
+
+    return index()
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- geocoding/get_coords.py
+++ geocoding/get_coords.py
@@ -8,7 +8,7 @@ from get_collection import get_collection
 
 collection_code = "FG"
 
-field_names = ["ID", "Project", "Normal Address", "Latitude", "Longitude", "Link"]
+field_names = ["ID", "Project", "Address", "City", "State", "County", "Country", "Latitude", "Longitude", "Link", "Validation"]
 geocoded_collection = []
 
 collection_df = get_collection(collection_code)
@@ -17,22 +17,31 @@ for index, row in tqdm(collection_df.iterrows(), total=len(collection_df)):
     row_dict = {}
     unique_id = row["unique ID"]
     project_name = row["Project name"]
+    city = row["City"]
+    state = row["State/Province"]
+    county = row["County"]
+    country = row["Country"]
     #country_code = get_country_code(row["Country"])                # Turn on if using GeoApify for more accurate results
-    if row["Street address"] == "" and row["City"] == "" and row["State/Province"] == "" and row["Country"] == "":
+    if row["Street address"] == "" and city == "" and state == "" and county == "" and country == "":
         latitude = ""
         longitude = ""
         link = ""
     else:
-        address_list = [row["Project name"], row["Street address"], row["City"], row["County"], row["State/Province"], row["Country"]]
+        address_list = [project_name, row["Street address"], city, county, state, country]
         address = [i for i in address_list if i != ""]
         address = ", ".join(address_list)
         normal_address, latitude, longitude, link = geocode_complete(address)
     row_dict["ID"] = unique_id
     row_dict["Project"] = project_name
-    row_dict["Normal Address"] = normal_address
+    row_dict["Address"] = normal_address
+    row_dict["City"] = city
+    row_dict["State"] = state
+    row_dict["County"] = county
+    row_dict["Country"] = country
     row_dict["Latitude"] = latitude
     row_dict["Longitude"] = longitude
     row_dict["Link"] = link
+    row_dict["Validation"] = ""
     geocoded_collection.append(row_dict)
 
 with open(collection_code + '.csv', 'w') as csvfile:
